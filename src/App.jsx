aca1749153393d170d4d7a876b01b5ed5b581c68
Nelson Toro <nelson.toro.flores@gmail.com>
2024-01-27 21:09:36 -0400

    Ways to use UseRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './App.css'
-
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 
 function App() {
   const { movies } = useMovies()
-  const [query, setQuery] = useState()
+  const [query, setQuery] = useState('')
+  const [error, setError] = useState(null)
+
+  // Value that will persist through many renderings
+  const counter = useRef(0)
+  counter.current++
+  console.log(counter)
+
+  // Value that will NOT persist through many renderings
+  let i = 0 // will be created and assigned to zero on each rendering
+  i++
+  console.log(i)
 
   // Uncontrolled way to get data from a form
   // It's uncontrolled because we use directly the DOM object without using React
@@ -30,7 +40,39 @@ function App() {
   // commonly used for validations
   const handleChange = (event) => {
     // target will be the input control
-    setQuery(event.target.value)
+    const newQuery = event.target.value
+    // Pre validation. It will avoid updating the query state if a space was typed
+    if (newQuery === ' ') {
+      return
+    }
+
+    setQuery(newQuery)
+
+    if (newQuery === '') {
+      setError('Please specify a Movie to search')
+      return
+    }
+
+    if (newQuery.match(/^\d+$/)) {
+      setError('You cannot search movies that starts with a number')
+      return
+    }
+
+    if (newQuery.length < 3) {
+      setError('Please specify at least three characters to search a movie')
+      return
+    }
+
+    setError(null)
+  }
+
+  useEffect(() => {}, [query])
+
+  // Using useRef referencing a component
+  const inputRef = useRef()
+  const handleClick = () => {
+    const value = inputRef.current.value // always to current to access its last value
+    alert(value)
   }
 
   return (
@@ -47,8 +89,17 @@ function App() {
           />
           {/* Uncontroled way */}
           <input name='query' placeholder='Avenger, Matrix, Star Wars...' />
-          <button type='submit'>Search</button>
+          {/* Controled Way using UseRef */}
+          <input
+            ref={inputRef}
+            name='queryUseRef'
+            placeholder='Avenger, Matrix, Star Wars...'
+          />
+          <button onClick={handleClick} type='submit'>
+            Search
+          </button>
         </form>
+        {error && <p className='error'>{error}</p>}
       </header>
       <main>
         <Movies movies={movies} />
